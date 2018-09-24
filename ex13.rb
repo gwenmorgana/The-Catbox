7ecc221b3399d49f8f391526f85de4ffac6f6f09
gwenmorgana <gwenmorgana@gmail.com>
2018-09-23 22:19:25 -0700

    Revert "Initial upload of my studies."
    This reverts commit 724927f0cf2ffd3be50d776cdb4b79e7151512b3.

--- ex13.rb
+++
@@ -1,5 +0,0 @@
-first, china, third = ARGV
-
-puts "Your first variable is: #{first}"
-puts "Your second variable is: #{china}"
-puts "Your third variable is: #{third}"
--- ex3calc.rb
+++
@@ -1,9 +0,0 @@
-puts "My Medication Tracker."
-
-puts "I take #{1 + 1 + 1 + 1} pills per day."
-puts "I Take one of these pills as needed."
-puts "If I take the second dose as needed, i will take #{1 + 1 + 1 + 2} pills per day."
-
-puts "In a 30 day period, I take #{(1 + 1 + 1 + 1) * 30} pills total."
-
-puts "Ten days into the month, I have #{(1 + 1 + 1 + 1) * 30 - (1 + 1 + 1 + 1) * 10} pills remaining."
