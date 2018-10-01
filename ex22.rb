1702268ba3ccd82f29f9a77c434380a4cbca6bc8
gwenmorgana <gwenmorgana@gmail.com>
2018-09-30 17:13:46 -0700

    My chapter 22 test completed. I need to make sure all code is in memory.

--- ex22.rb
+++ ex22.rb
@@ -10,6 +10,10 @@ puts "PEMDAS means Parenthesis, Exponents, Multiplication, Division, Addition, a
 # i can set variables like this. Variables cant start with a number, but letters and numbers okay N1 for example
 cat = "King Tommen" # This variable stores a strings
 cats = 12 # This variable sets the number of cats as 12
+# How do i make more cats?
+cats = cats + 1
+# That is long, any less?
+cats -= 1 # also can use += as well.
 are_there_cats = false # this is a Boolean
 cats_fp = 12.0 # add .0 to set the number as floating point
 
@@ -29,3 +33,71 @@ puts %{ blah blah
   cats = 11? huh?
 
 }
+puts "." * 10 # Multiple lines from one line...
+print "I"; print "can also"; print "use multiple lines to print"
+# can also make big quotes...
+puts %q{
+There's something going on here.
+
+With this weird quote
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+
+You can put escape sequences in "", but not in a ''
+ }
+ # I have learned about escape sequences...
+# This generates a QUOTE...  IT CAN BE MULTILINE!
+ puts %q{
+ These are the escape sequences in Ruby...
+\\ = backslash
+\' = single quote
+\" double quote
+\  = ASCII bell (BEL)
+\b = backspace (BS)
+\f = ASCII formfeed
+\r = ASCII carriage return
+\t = ASCII Horizontal tab
+\uxxxx = Character with 16 bit hex value (Unicode only)
+\v = ASCII Vertical tab
+\ooo = Character with octal value ooo
+\xhh = Character with hex value hh
+}
+puts """
+I can quote in here and still use escape sequences... \n
+Oh... and i cause use a variable...
+How many cats again? #{cats}
+"""
+# when using $stdin.gets.chomp, i can also add .to_f to convert (to floating point) or .to_i (for integer)
+# I can grab variables from the command line (i.e) ruby file.rb var1 var2 by doing this...
+# var1, var2, var3 = ARGV
+# But you have to use $stdin when using ARGV!
+
+# so how do I open files? (remember, you must close a file after opening it for memory sake..)
+# txt = open(filename, 'w')   # <----- able to write to the file vs read only.
+# print txt.read
+# txt.close
+
+# also, don't forget closing a file after usage in a temporary block...
+
+# from_file, to_file = ARGV
+# This line uses inline syntax for blocks, everything inside the { } will be destroyed then the open function returns
+# Using this I dont need to close 2 files - the |f| is saying to yield the variable f inside of the blocks
+# The block syntax will auto close
+# newdata = File.open(from_file) { |f| f.read() }
+#output = File.open(to_file, 'w')
+#output.write(newdata)
+#output.close
+# -------------------------------------------------------------------
+# So how do i define a function?
+def lunch(drink, food)
+  puts "This explains my function, until i end it..."
+  puts "What is for lunch? We have #{drink} and some #{food}"
+end
+
+# How can I use the function
+# put the variables directly in...?
+drink = "Iced Tea"
+food = "Chicken Burger"
+
+# Or i can pass variables in place of the strings... or numbers?
+# lunch(20, 10)
