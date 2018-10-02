bd3a511a26c11a43a97fc9c08b5eb5133c292f3f
gwenmorgana <gwenmorgana@gmail.com>
2018-10-01 22:32:19 -0700

    New lesson updates.

---
+++ ex23.rb
@@ -0,0 +1,4 @@
+# This lesson isnt code at all.
+# I am assigned by the book to read ruby code and learn
+# Tonight I will look up source and learn new things.
+puts "It is time to learn something new!"
