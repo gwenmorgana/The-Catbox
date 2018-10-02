4316611e27724a04eb1b51f33a13de034047d6a8
gwenmorgana <gwenmorgana@gmail.com>
2018-10-01 22:34:08 -0700

    Found a cool EDU site.

--- ex23.rb
+++ ex23.rb
@@ -2,3 +2,4 @@
 # I am assigned by the book to read ruby code and learn
 # Tonight I will look up source and learn new things.
 puts "It is time to learn something new!"
+puts "I found a cool site, I will explore some examples at http://sandbox.mc.edu/~bennet/ruby/code/index.html"
