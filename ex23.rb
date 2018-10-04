8be1d7258053084ec0c9c910c39654857f46d983
gwenmorgana <gwenmorgana@gmail.com>
2018-10-04 11:31:24 -0700

    Second researched code for if else

--- ex23.rb
+++ ex23.rb
@@ -3,3 +3,8 @@
 # Tonight I will look up source and learn new things.
 puts "It is time to learn something new!"
 puts "I found a cool site, I will explore some examples at http://sandbox.mc.edu/~bennet/ruby/code/index.html"
+
+# I am also exploring Ruby code examples in Github.
+# Though it has not been covered yet, I am looking at IF, ELSIF, ELSE, and DEF to form menu systems. and direct the flow of my programs.
+
+# https://github.com/buzzamus/text-RPG/blob/master/game.rb Has some insight on the above functions
