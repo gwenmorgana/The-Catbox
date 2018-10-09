df7df56765e68c3cdacd6bcbae41965df554da3d
gwenmorgana <gwenmorgana@gmail.com>
2018-10-08 17:07:08 -0700

    Learning about push/pop and sorting functions

--- ex25.rb
+++ ex25.rb
@@ -1,23 +1,27 @@
 module Ex25
 
   # This function will break up words for us.
+# The reverse of the .split function is .join
   def Ex25.break_words(stuff)
     words = stuff.split(' ')
     return words
   end
 
   # Sorts the words.
+  # Sort can also call .sort_by(&:length)
   def Ex25.sort_words(words)
     return words.sort
   end
 
   # Prints the first word after shifting it off.
+  # Shift can also move variables contained in an array
   def Ex25.print_first_word(words)
     word = words.shift
     puts word
   end
 
   # Prints the last word after popping it off.
+  # Push and pop will add and remove variables into arrays
   def Ex25.print_last_word(words)
     word = words.pop
     puts word
