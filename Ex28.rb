6fef115c106e460a410cd99e0d6dd14be4b19f99
gwenmorgana <gwenmorgana@gmail.com>
2018-10-13 13:52:57 -0700

    Updates for my studies

--- Ex28.rb
+++ Ex28.rb
@@ -1,21 +1,21 @@
 puts "Welcome to Boolean practice."
 
-true && true
-false && false
-1 == 1 && 2 == 1
-"test" == "test"
-1 == 1 || 2 != 1
-true && 1 == 1
-false && 0 != 1
-true || 1 == 1
-"test" == "testing"
-1 != 0 && 2 == 1
-"test" == 1
-!(true && false)
-!(1 == 1 && 0 != 1)
-!(10 == 1 || 1000 == 1000)
-!(1 != 10 || 3 == 4)
-!("testing" == "testing" && "Richard" == "Cool Guy")
-1 == 1 && (!("testing" == 1 || 1 == 0))
-"chunky" == "bacon" && (!(3 == 4 || 3 == 3))
-3 == 3 && (!("testing" == "testing" || "Ruby" == "Fun"))
+true && true # true
+false && false # false
+1 == 1 && 2 == 1 # true
+"test" == "test" # true
+1 == 1 || 2 != 1 # true
+true && 1 == 1 # true
+false && 0 != 1 # false
+true || 1 == 1 # true
+"test" == "testing" # false
+1 != 0 && 2 == 1 # true
+"test" == 1 # false
+!(true && false) # true
+!(1 == 1 && 0 != 1) # false
+!(10 == 1 || 1000 == 1000) # false
+!(1 != 10 || 3 == 4) # 
+!("testing" == "testing" && "Richard" == "Cool Guy") #
+1 == 1 && (!("testing" == 1 || 1 == 0)) #
+"chunky" == "bacon" && (!(3 == 4 || 3 == 3)) #
+3 == 3 && (!("testing" == "testing" || "Ruby" == "Fun")) #
