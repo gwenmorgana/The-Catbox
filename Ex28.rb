725420f2abb1af7869a243d8899b36cb97984957
gwenmorgana <gwenmorgana@gmail.com>
2018-10-12 23:17:21 -0700

    Ex28, reading Boolean logic.

---
+++ Ex28.rb
@@ -0,0 +1,21 @@
+puts "Welcome to Boolean practice."
+
+true && true
+false && false
+1 == 1 && 2 == 1
+"test" == "test"
+1 == 1 || 2 != 1
+true && 1 == 1
+false && 0 != 1
+true || 1 == 1
+"test" == "testing"
+1 != 0 && 2 == 1
+"test" == 1
+!(true && false)
+!(1 == 1 && 0 != 1)
+!(10 == 1 || 1000 == 1000)
+!(1 != 10 || 3 == 4)
+!("testing" == "testing" && "Richard" == "Cool Guy")
+1 == 1 && (!("testing" == 1 || 1 == 0))
+"chunky" == "bacon" && (!(3 == 4 || 3 == 3))
+3 == 3 && (!("testing" == "testing" || "Ruby" == "Fun"))
