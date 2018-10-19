b895e8d6095423bf76482af78b9aa5208b32968b
gwenmorgana <gwenmorgana@gmail.com>
2018-10-18 23:44:30 -0700

    Working with arrays and cardinal versus ordinal numbers

---
+++ ex34.rb
@@ -0,0 +1,24 @@
+# accessing elements of arrays
+
+# Ordinal versus Cardinal numbers.
+# Ordinal == ordered, 1st.
+# Cardinal == cards at random, 0
+
+
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+# The animal at 1
+e1 = animals[1]
+# The third (3rd) animal.
+e2 = animals[2]
+e4 = animals[3]
+# The fifth (5th) animal.
+e5 = animals[5]
+# The animal at 2.
+e6 = animals[2]
+# The sixth (6th) animal
+e7 = animals[5]
+# The animal at 4
+e8 = animals[4]
