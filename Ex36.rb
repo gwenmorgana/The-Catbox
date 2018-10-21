584b5286ecab7066e5e5e39fbe106dd41e862786
gwenmorgana <gwenmorgana@gmail.com>
2018-10-20 19:46:43 -0700

    Ex36.rb rules for construction of my game for Lesson 36

---
+++ Ex36.rb
@@ -0,0 +1,20 @@
+# Designing and Debugging
+
+puts "Rules for If-Statements\n"
+puts "1.) Every if-statement must have an else"
+puts "2.) If this else should never run because it doesn't make sense, then you must use a die function in the else that prints out an error message and dies, just like we did in the last exercise. This will find many errors."
+puts "3.) Never nest if-statements more than two deep and always try to do them one deep."
+puts "4.) Treat if-statements like paragraphs, where each if-elsif-else group int is like a set of sentences. Put blank lines before and after."
+puts "5.) Your Boolean tests should be simple. If they are complex, move their calculations to variables earlier in your function and use a good name for the variable."
+puts "\n\n\n"
+puts "Rules for Loops\n"
+puts "1.) Use a while-loop ony to loop forever, and that means probably never. This only applies to Ruby; other languanges are different."
+puts "2.) Use a for-loop for all other kinds of looping, especially if there is a fixed or limited number of things to loop over."
+puts "\n\n\n"
+puts "Tips for Debugging.\n"
+puts "1.) Do not use a \"debugger.\" A debugger is like doing a full-body scan on a sick person. You do not get any specific useful information, and you will find a whole lot of inforation that doesn't help and is just confusing."
+puts "2.) The best way to debug a program is to use put to print out values of variables at points in the program to see where they go wrong."
+puts "3.) Make sure parts of your programs work as you work on them. Do not write massive files of code before you try to run them. Code a little, run a little, fix a little."
+
+puts "\n\n"
+puts "For this lesson, I will construct a game completely, and post the plan in which I will code it."
