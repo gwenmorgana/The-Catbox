23693c611db4cffd98576ebae9dfc1cb7a7fcb43
gwenmorgana <gwenmorgana@gmail.com>
2018-10-22 21:50:13 -0700

    Files updated for game project ch36

--- SkoomaQuest.rb
+++ SkoomaQuest.rb
@@ -1,43 +1,10 @@
-# SkoomaQuest. Ver 1 - By Richard Grady
-# Join the Khajiit on a mission to get a fix!
+# Welcome to SkoomaQuest. A Game by Richard Grady.
 
-def menu
-  
+# include CombatEngine.rb
 
-
-# Player attributes
-sta = 25
-int = 25
-str = 25
-level = 1
-hps = level * sta / 2
-mana = level * int / 2
-
-# system
-prompt = "SQ:> "
-
-
-# Game intro
-puts "Welcome to SkoomaQuest!\n\n"
-puts "You open your eyes and find yourself in a jail cell."
-puts "yada yada... more lore here...\n\n"
-
-puts "What is your name?"
-
-print prompt
-name = $stdin.gets.chomp
-
-print "Hello, #{name}\n"
-puts "You can call me your inner-voice, a narrator you could say.\n"
-puts "I will guide you on your mission to get what you need."
-puts "You can ask me a number of commands, for example..."
-puts "help, .....etc"
-
-command = $stdin.gets.chomp
-if command == 'help'
-  print "This is the help screen." # I wonder, can I make an if call a custom function i define?
-elsif command == 'cats'
-    print "Mrrow."
-else
-  print "That is not a valid command, sire."
-end
+def main_menu
+  puts "Intro Text"
+  puts "1.) New Game."
+  puts "2.) Quit."
+  choice = $stdin.gets.chomp
+  if choice == "1"
