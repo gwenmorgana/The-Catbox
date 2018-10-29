19039cd5596d0045aaf3744b4a8eff56086cd1bb
gwenmorgana <gwenmorgana@gmail.com>
2018-10-28 17:52:24 -0700

    UPdate to Scratchpad1 this tile pushes data from variable into strings.

--- Scratchpad1.rb
+++ Scratchpad1.rb
@@ -1,14 +1,16 @@
 # Experimental work with strings..
 # I want to parse CSV style strings with $ instead of ,
+# This program will push variables into the array.
 
-ten_things = "Field1 Field2 Field3 Field4 Field5 Field6"
+ten_things = "OldData1$OldData1$OldData3"
+# ten_things = "Field1 Field2 Field3 Field4 Field5 Field6"
 more_stuff_2 = "18:00:01$Catbox has joined the Raid."
-more_stuff = ["Alpha", "Beta", "Gamma", "Delta", "Epsilon", "Zeta", "Eta", "Theta", "Iota", "Kappa"]
+more_stuff = ["Alpha", "Beta", "Gamma", "Delta", "Epsilon", "Zeta", "Eta", "Theta", "Iota", "Kappa", "Lambda", "Mu", "Nu"]
 
 
-stuff  = ten_things.split(' ')
+stuff  = ten_things.split('$')
 
-while stuff.length != 10
+while stuff.length != 11
   next_one = more_stuff.pop
   puts "Adding: #{next_one}"
   stuff.push(next_one)
