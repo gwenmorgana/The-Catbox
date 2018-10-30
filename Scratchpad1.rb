c62f6fd0278a528778e34b4df817bfff47d0a3bb
gwenmorgana <gwenmorgana@gmail.com>
2018-10-29 22:21:57 -0700

    Parser update

--- Scratchpad1.rb
+++ Scratchpad1.rb
@@ -2,7 +2,7 @@
 # I want to parse CSV style strings with $ instead of ,
 # This program will push variables into the array.
 
-ten_things = "OldData1$OldData1$OldData3"
+ten_things = "OldData1$OldData2$OldData3"
 # ten_things = "Field1 Field2 Field3 Field4 Field5 Field6"
 more_stuff_2 = "18:00:01$Catbox has joined the Raid."
 more_stuff = ["Alpha", "Beta", "Gamma", "Delta", "Epsilon", "Zeta", "Eta", "Theta", "Iota", "Kappa", "Lambda", "Mu", "Nu"]
