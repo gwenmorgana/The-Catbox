181747d0e2ae0f38af36b2ac71b8674fa7aea1d2
gwenmorgana <gwenmorgana@gmail.com>
2018-10-30 21:38:19 -0700

    More work with Hashes

--- Scratchpad1.rb
+++ Scratchpad1.rb
@@ -2,6 +2,9 @@
 # I want to parse CSV style strings with $ instead of ,
 # This program will push variables into the array.
 
+# available tools. close, read, readline, truncate, write('stuff'), seek(0)
+
+log_sample = "[Mon Oct 29 17:41:52 2018] Catbox joined the raid."
 ten_things = "OldData1$OldData2$OldData3"
 # ten_things = "Field1 Field2 Field3 Field4 Field5 Field6"
 more_stuff_2 = "18:00:01$Catbox has joined the Raid."
