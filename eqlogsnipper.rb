ceef09cce4b5aebe6834af5965921d74bb7068c9
gwenmorgana <gwenmorgana@gmail.com>
2018-11-09 14:16:19 -0800

    UPdates

--- eqlogsnipper.rb
+++ eqlogsnipper.rb
@@ -1,5 +1,7 @@
 # eqlog text sample formatting - CSV style using spaces
 # [Tue Aug 21 14:36:19 2018] You feel different.
+
+=begin
 from_file, to_file = ARGV
 
 puts "Welcome to EQ Log Snipper v1"
@@ -22,6 +24,25 @@ out_file = open(to_file, 'w')
 out_file.write(indata)
 
 puts "File copy complete."
-
 out_file.close
 in_file.close
+=end
+
+
+##### Experimental section - Trying to search file with regex
+is_proper_date = false
+File.open("explog.txt", 'r') do |file| # change processed.txt to explog.txt for testing purposes...
+  file.readlines.each do |line|
+
+    if is_proper_date == true && (line.match(/^  (^)/) || line.strip.empty?)
+      is_proper_date = false
+    end
+
+    is_proper_date = true if line.match("Thu")
+
+    puts line if is_proper_date == true
+  end
+end
+
+
+##### End experimental section
