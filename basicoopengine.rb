f628cac5e74f9f0aa67fc79dd7e12ed39cfc3f0d
gwenmorgana <gwenmorgana@gmail.com>
2018-12-17 21:38:52 -0800

    Create basicoopengine.rb

---
+++ basicoopengine.rb
@@ -0,0 +1,81 @@
+class Scene
+  def enter()
+    puts "This scene is not yet configured. Subclass it and implement enter()."
+
+  end
+end
+
+class Engine
+
+  def initialize(scene_map)
+    @scene_map = scene_map
+  end
+
+  def play()
+    current_scene = @scene_map.opening_scene()
+    last_scene = @scene_map.next_scene('finished')
+
+    while current_scene != last_scene
+      next_scene_name = current_scene.enter()
+      current_scene = @scene_map.next_scene(next_scene_name)
+    end
+    # be sure to print out the last scene
+    current_scene.enter()
+  end
+end
+
+class Death < Scene
+
+  @@quips = [
+    "Death 1.",
+    "Death 2.",
+    "Death 3.",
+    "Death 4."
+
+  ]
+
+  def enter()
+    puts @@quips[rand(0..(@@quips.length -1))]
+    exit(1)
+  end
+end
+
+class CentralCorridor < Scene
+
+  def enter()
+  end
+end
+
+class LaserWeaponArmory < Scene
+
+  def enter()
+  end
+end
+
+class TheBridge < Scene
+
+  def enter()
+  end
+end
+
+class EscapePod < Scene
+
+  def enter()
+  end
+end
+
+class Map
+
+  def initialize(start_scene)
+  end
+
+  def next_scene(scene_name)
+  end
+
+  def opening_scene()
+  end
+end
+
+a_map = Map.new('central_corridor')
+a_game = Engine.new(a_map)
+a_game.play()
